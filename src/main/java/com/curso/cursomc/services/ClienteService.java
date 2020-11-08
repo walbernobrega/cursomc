a1b9a014bf06604b71a93558209711044118e5b7
Walber Nobrega <walber@santanatextiles.com>
2020-11-08 17:59:39 -0300

    Endpoint /clientes/{1} disponivel

---
+++ src/main/java/com/curso/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.curso.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.cursomc.domain.Cliente;
+import com.curso.cursomc.repositories.ClienteRepository;
+import com.curso.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
