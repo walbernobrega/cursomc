284b72b9f598406a9566eaf05be4f14d3e4ff33b
Walber Nobrega <walber@santanatextiles.com>
2020-12-25 18:06:41 -0300

    Atualizacao utilizando somento JsonIgnore

--- src/main/java/com/curso/cursomc/domain/Pedido.java
+++ src/main/java/com/curso/cursomc/domain/Pedido.java
@@ -29,11 +29,9 @@ public class Pedido implements Serializable {
 	private Date instante;
 	
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
