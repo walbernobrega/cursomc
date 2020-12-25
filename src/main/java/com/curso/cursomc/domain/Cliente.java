77b428ac50cfaa88a86b473ce9158044949f9097
Walber Nobrega <walber@santanatextiles.com>
2020-12-25 17:55:42 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/curso/cursomc/domain/Cliente.java
+++ src/main/java/com/curso/cursomc/domain/Cliente.java
@@ -17,6 +17,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.curso.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
@@ -39,6 +40,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
