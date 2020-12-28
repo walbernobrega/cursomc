ed655eaf4bb3191fccdd9a9e1c5cc09591ce0119
Walber Nobrega <walber@santanatextiles.com>
2020-12-28 17:31:51 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/curso/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/curso/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.curso.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.cursomc.domain.Categoria;
+import com.curso.cursomc.dto.CategoriaDTO;
 import com.curso.cursomc.services.CategoriaService;
 
 @RestController
@@ -51,4 +54,14 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		
+		
+		return ResponseEntity.ok().body(listDto);
+	}
+	
 }
