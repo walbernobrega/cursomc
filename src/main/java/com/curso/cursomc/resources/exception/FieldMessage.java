2915d4a74323c944beaf61cdeb9b4dd6c82ebf18
Walber Nobrega <walber@santanatextiles.com>
2021-02-20 18:37:10 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/curso/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,38 @@
+package com.curso.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String messagem;
+	
+	public FieldMessage() {
+		
+	}
+
+	public FieldMessage(String fieldName, String messagem) {
+		super();
+		this.fieldName = fieldName;
+		this.messagem = messagem;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessagem() {
+		return messagem;
+	}
+
+	public void setMessagem(String messagem) {
+		this.messagem = messagem;
+	}
+	
+
+}
