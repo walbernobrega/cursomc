bc69448b595c1df30d131472f33ec2f487163c57
walbernobrega <walber@santanatextiles.com>
2021-03-21 17:11:08 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/curso/cursomc/config/TestConfig.java
+++ src/main/java/com/curso/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.curso.cursomc.services.DBService;
+import com.curso.cursomc.services.EmailService;
+import com.curso.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -20,6 +22,11 @@ public class TestConfig {
 	public boolean instantiateDataBase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean 
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 
 }
---
+++ src/main/java/com/curso/cursomc/services/EmailService.java
@@ -0,0 +1,13 @@
+package com.curso.cursomc.services;
+
+import org.springframework.mail.SimpleMailMessage;
+
+import com.curso.cursomc.domain.Pedido;
+
+public interface EmailService {
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+	
+	void sendEmail(SimpleMailMessage msg);
+
+}
