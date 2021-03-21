63448d6c0b875889b078682a9c48ffc2bc362d05
walbernobrega <walber@santanatextiles.com>
2021-03-21 18:50:34 -0300

    Email HTML

--- src/main/java/com/curso/cursomc/services/EmailService.java
+++ src/main/java/com/curso/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.curso.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.curso.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
-
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
+	
 }
