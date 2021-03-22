6a6ed790df93dc66050d29da35f00bd34ed7c2eb
walbernobrega <walber@santanatextiles.com>
2021-03-22 17:15:58 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/curso/cursomc/services/PedidoService.java
+++ src/main/java/com/curso/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.curso.cursomc.domain.Cliente;
 import com.curso.cursomc.domain.ItemPedido;
 import com.curso.cursomc.domain.PagamentoComBoleto;
 import com.curso.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.curso.cursomc.domain.enums.EstadoPagamento;
 import com.curso.cursomc.repositories.ItemPedidoRepository;
 import com.curso.cursomc.repositories.PagamentoRepository;
 import com.curso.cursomc.repositories.PedidoRepository;
+import com.curso.cursomc.security.UserSS;
+import com.curso.cursomc.services.exceptions.AuthorizationException;
 import com.curso.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,4 +76,21 @@ public class PedidoService {
 		return obj;
 		
 	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return repo.findByCliente(cliente, pageRequest);
+		
+	}
+
 }
