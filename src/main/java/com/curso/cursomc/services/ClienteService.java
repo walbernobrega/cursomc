9c322b98bfd562dd90df8f2c0d57031c106c8a1f
walbernobrega <walber@santanatextiles.com>
2021-03-23 17:00:13 -0300

    Bonus ajustando o tamanho da imagem

--- src/main/java/com/curso/cursomc/services/ClienteService.java
+++ src/main/java/com/curso/cursomc/services/ClienteService.java
@@ -51,6 +51,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
 		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
@@ -130,6 +133,9 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multiPartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage,  size);
+		
 		String fileName = prefix + user.getId() + ".jpg";
 		
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
